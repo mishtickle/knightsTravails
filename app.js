f266cbedfd7f3ffa514645a4a1dc7d417a070016
Hamish <hamishok@gmail.com>
2023-03-20 11:38:22 +1300

    working app

---
+++ app.js
@@ -0,0 +1,82 @@
+// function gameBoard() {
+//     let theGameboard = []
+//     for (let i = 1; i < 9; i++){
+//         for (let j = 1; j < 9; j++){
+//             theGameboard.push([i,j]);
+//         }
+//     }
+//     return theGameboard;
+// }
+
+
+
+let possibleKnightsMoves = [[1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1], [-1, 2]];
+
+class knightNode {
+    constructor(xpos, ypos){
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.adjacents = [];
+        this.route = [];
+    }
+}
+
+let visited = [];
+let queue = [];
+
+function knightMoves(startPosition, endPosition){
+    let knight = new knightNode(startPosition[0], startPosition[1]);
+    knight.route.push([knight.xpos, knight.ypos]);
+    visited.push([knight.xpos, knight.ypos]);
+    queue.push(knight);
+    let boolean = isValid(knight.xpos, knight.ypos);
+    while (queue.length > 0){
+        let current = queue.shift();
+        for (let i = 0; i < possibleKnightsMoves.length; i++){
+            let next = new knightNode(current.xpos + possibleKnightsMoves[i][0], current.ypos + possibleKnightsMoves[i][1]);
+            for (let k = 0; k < current.route.length; k++){
+                next.route.push(current.route[k]);
+            }
+            if (isValid(next.xpos, next.ypos)){
+                if (!isVisited(next.xpos, next.ypos)){
+                    next.route.push([next.xpos, next.ypos])
+                    queue.push(next);
+                    visited.push([next.xpos, next.ypos]);
+                    if (endPosition[0] == next.xpos && endPosition[1] == next.ypos){
+                        console.log(`You made it in ${next.route.length - 1} moves`)
+                        console.log(next.route);
+                        return;        
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
+}
+
+function isValid(xpos, ypos){
+    if (xpos > 0 && xpos <= 8){
+        if (ypos > 0 && ypos <= 8){
+            return true;
+        }
+    }
+    return false;
+}
+
+function isVisited(xpos, ypos){
+    for (let i = 0; i < visited.length; i++){
+        if (visited[i][0] == xpos && visited[i][1] == ypos){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+knightMoves([1,1], [8,8])
+
+
+
+// let theGameboard = gameBoard();
